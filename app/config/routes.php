07308f884c4d89314dc052d906201be2f91ce320
Web-yoga <hyehaeng@gmail.com>
2016-10-05 16:25:47 +0300

    Два роута для отдельной новости и на лист новостей

--- app/config/routes.php
+++ app/config/routes.php
@@ -4,5 +4,7 @@ return array(
     '' => 'site/index/Главная',
     'registration' => 'user/registration/Регистрация',
     'login' => 'user/login/Вход',
-    'search' => 'site/search/Поиск'
+    'search' => 'site/search/Поиск',
+    'news/[0-9]+' => 'news/news/Новости', 
+    'news' => 'news/newslist/Новости' 
 );
