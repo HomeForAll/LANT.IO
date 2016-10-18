61e04aa6e7f441bf7d40f5eb0804a877f023b0b2
Дмитрий <badarancha.dmitry@gmail.com>
2016-10-18 21:12:15 +0300

    Удалил composer

--- app/models/AuthModel.php
+++ app/models/AuthModel.php
@@ -263,19 +263,7 @@ class AuthModel
 
     private function goo()
     {
-        $client = new Google_Client();
-        $client->setAuthConfig(ROOT_DIR . '/app/config/google_client_secrets.json');
-        $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
 
-        if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
-            $client->setAccessToken($_SESSION['access_token']);
-            $drive_service = new Google_Service_Drive($client);
-            $files_list = $drive_service->files->listFiles(array())->getItems();
-            echo json_encode($files_list);
-        } else {
-            $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
-            header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-        }
     }
 
     private function fb()
--- index.php
+++ index.php
@@ -7,7 +7,6 @@ defined('CONFIG_DIR') or define('CONFIG_DIR', ROOT_DIR . '/app/config/');
 defined('SERVER') or define('SERVER', 'nginx'); // 'apache' or 'nginx'
 defined('STATUS') or define('STATUS', '1'); // NORMAL: 0; DEV: 1;
 
-require_once __DIR__ . '/app/composer/vendor/autoload.php';
 require_once __DIR__ . '/app/core/Loader.php';
 
 spl_autoload_register(array('Loader', 'classLoad'));
