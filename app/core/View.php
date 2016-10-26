bd7b2f81c3fa88a0601f62b4d995836a6cd5d61b
Дмитрий <badarancha.dmitry@gmail.com>
2016-10-26 18:55:11 +0300

    Немного изменений View

--- app/core/View.php
+++ app/core/View.php
@@ -4,41 +4,41 @@ class View {
     private $title;
     private $template;
     private $content;
-    private $data;
+    public $data;
 
     public function __construct($template) {
         $this->template = $template;
     }
 
     public function render($view, $data = null) {
-        $this->content = self::getContent($view);
         $this->data = $data;
-        $this->renderLayout($this->template);
+        $this->content = self::getContent($view);
+        self::renderLayout();
     }
 
     private function renderHead() {
-        require(ROOT_DIR . '/templates/' . $this->template . '/meta.php');
-        require(ROOT_DIR . '/templates/' . $this->template . '/links.php');
-        require(ROOT_DIR . '/templates/' . $this->template . '/css.php');
-        require(ROOT_DIR . '/templates/' . $this->template . '/js.php');
+        include(ROOT_DIR . '/templates/' . $this->template . '/meta.php');
+        include(ROOT_DIR . '/templates/' . $this->template . '/links.php');
+        include(ROOT_DIR . '/templates/' . $this->template . '/css.php');
+        include(ROOT_DIR . '/templates/' . $this->template . '/js.php');
     }
 
     private function renderBody() {
-        require(ROOT_DIR . '/templates/' . $this->template . '/begin.php');
-        require(ROOT_DIR . '/templates/' . $this->template . '/header.php');
+        include(ROOT_DIR . '/templates/' . $this->template . '/begin.php');
+        include(ROOT_DIR . '/templates/' . $this->template . '/header.php');
         echo $this->content;
-        require(ROOT_DIR . '/templates/' . $this->template . '/footer.php');
-        require(ROOT_DIR . '/templates/' . $this->template . '/end.php');
+        include(ROOT_DIR . '/templates/' . $this->template . '/footer.php');
+        include(ROOT_DIR . '/templates/' . $this->template . '/end.php');
     }
 
     private function getContent($view) {
         ob_start();
         ob_implicit_flush(false);
-        require(ROOT_DIR . '/app/views/' . $view . '.php');
+        include(ROOT_DIR . '/app/views/' . $view . '.php');
         return ob_get_clean();
     }
 
-    private function renderLayout($layout) {
-        include ROOT_DIR . '/templates/layouts/' . $layout . '.php';
+    private function renderLayout() {
+        include(ROOT_DIR . '/templates/layouts/main.php');
     }
 }
