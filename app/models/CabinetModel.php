67cc3051e57667534e672fb9dc1b59eec5d76992
Дмитрий <badarancha.dmitry@gmail.com>
2016-10-27 17:51:35 +0300

    Шаблон

--- app/models/CabinetModel.php
+++ app/models/CabinetModel.php
@@ -19,7 +19,7 @@ class CabinetModel extends Model
         if (isset($_POST['handle'])) {
             if (isset($_POST['sendCheck'])) {
                 foreach ($_SESSION['keys'] as $email => $key) {
-                    $str = file_get_contents('email.html');
+                    $str = file_get_contents(ROOT_DIR . '/templates/layouts/email.php');
                     $phrase  = $str;
                     $old = array("KEY");
                     $new   = array($key);
---
+++ templates/layouts/email.php
@@ -0,0 +1,122 @@
+<div id=":2jr" class="a3s aXjCH m158001c1aab07477"><u></u>
+    
+    
+    
+    
+    
+    
+  
+
+
+  <div class="m_6317060884218229271no-padding" style="margin:0;padding:0">
+
+    <table class="m_6317060884218229271wrapper" style="border-collapse:collapse;table-layout:fixed;min-width:320px;width:100%;background-color:#131814" cellpadding="0" cellspacing="0"><tbody><tr><td>
+      <div class="m_6317060884218229271preheader" style="Margin:0 auto;max-width:560px;min-width:280px;width:280px;width:calc(28000% - 173040px)">
+        <div style="border-collapse:collapse;display:table;width:100%">
+        
+          <div class="m_6317060884218229271snippet" style="display:table-cell;Float:left;font-size:12px;line-height:19px;max-width:280px;min-width:140px;width:140px;width:calc(14000% - 78120px);padding:10px 0 5px 0;color:#5c6266;font-family:Roboto,Tahoma,sans-serif">
+            
+          </div>
+        
+          <div class="m_6317060884218229271webversion" style="display:table-cell;Float:left;font-size:12px;line-height:19px;max-width:280px;min-width:139px;width:139px;width:calc(14100% - 78680px);padding:10px 0 5px 0;text-align:right;color:#5c6266;font-family:Roboto,Tahoma,sans-serif">
+            
+          </div>
+        
+        </div>
+      </div>
+      <div class="m_6317060884218229271header" style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 173000px)" id="m_6317060884218229271emb-email-header-container">
+      
+        <div class="m_6317060884218229271logo m_6317060884218229271emb-logo-margin-box" style="font-size:26px;line-height:32px;Margin-top:0px;Margin-bottom:0px;color:#c3ced9;font-family:Bitter,Georgia,serif;Margin-left:20px;Margin-right:20px" align="center">
+          <div class="m_6317060884218229271logo-center" style="font-size:0px!important;line-height:0!important" align="center" id="m_6317060884218229271emb-email-header"><a style="text-decoration:none;color:#c3ced9" href="http://lantio.cmail20.com/t/d-l-djyuddt-bdulrydhh-r/" target="_blank" data-saferedirecturl="https://www.google.com/url?hl=ru&amp;q=http://lantio.cmail20.com/t/d-l-djyuddt-bdulrydhh-r/&amp;source=gmail&amp;ust=1477557075155000&amp;usg=AFQjCNEMaSWeODZrOsY9QgRjCwJvDd6oVw"><img style="height:auto;width:100%;border:0;max-width:200px" src="https://ci5.googleusercontent.com/proxy/dJsQM5xa2PcEbfrpl7mpnHWjDpWctt_LpioQCt02kKUPI-Qdl5UqjEteVqIvHjgwLrp_7wqudkg7HFfBNRWlXkHolJPc0m5deeBUASYqBgu9=s0-d-e1-ft#http://i1.cmail20.com/ei/d/2F/E67/67E/192233/csfinal/-2.png" alt="" width="200" class="CToWUd"></a></div>
+        </div>
+      
+      </div>
+      <div style="background-color:#131814;background-position:0px 0px;background-image:url(https://ci6.googleusercontent.com/proxy/xFFdsAKb4SFNO9QtZtUmARAlVANz3_hXvJxhfPs3MqWAnoPKihRIbO_dTqlJpAKf8pBbTKw-2h_iATfN6aNPC0cWUpOXgjZ4bCl9yC_bMuzIHGEnMuV_arVSTzP9CLBF=s0-d-e1-ft#http://i1.cmail20.com/ei/d/2F/E67/67E/192233/csfinal/image-beautiful-2.jpg);background-repeat:repeat">
+        <div class="m_6317060884218229271layout m_6317060884218229271one-col" style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 173000px);word-wrap:break-word;word-break:break-word">
+          <div class="m_6317060884218229271layout__inner" style="border-collapse:collapse;display:table;width:100%">
+          
+            <div class="m_6317060884218229271column" style="max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px);text-align:left;color:#a6b0b3;font-size:14px;line-height:21px;font-family:Roboto,Tahoma,sans-serif">
+            
+              <div style="Margin-left:20px;Margin-right:20px">
+      <div style="line-height:25px;font-size:1px">&nbsp;</div>
+    </div>
+            
+              <div style="Margin-left:20px;Margin-right:20px">
+      <h2 class="m_6317060884218229271size-24" style="Margin-top:0;Margin-bottom:0;font-style:normal;font-weight:normal;color:#fff;font-size:20px;line-height:28px;font-family:Avenir,sans-serif;text-align:center" lang="x-size-24">WELCOME TO</h2><h1 class="m_6317060884218229271size-48" style="Margin-top:16px;Margin-bottom:20px;font-style:normal;font-weight:normal;color:#fff;font-size:36px;line-height:43px;font-family:Avenir,sans-serif;text-align:center" lang="x-size-48">ALPHA TEST</h1>
+    </div>
+            
+              <div style="Margin-left:20px;Margin-right:20px">
+      <div style="line-height:40px;font-size:1px">&nbsp;</div>
+    </div>
+            
+              <div style="Margin-left:20px;Margin-right:20px">
+      <div style="line-height:20px;font-size:1px">&nbsp;</div>
+    </div>
+            
+              <div style="Margin-left:20px;Margin-right:20px">
+      <p class="m_6317060884218229271size-44" style="Margin-top:0;Margin-bottom:20px;font-size:34px;line-height:43px;text-align:center" lang="x-size-44"><strong><span style="color:#ffffff">QYKE-5PQW-PLPZ-2MH3</span></strong></p>
+    </div>
+            
+              <div style="Margin-left:20px;Margin-right:20px">
+      <div style="line-height:60px;font-size:1px">&nbsp;</div>
+    </div>
+            
+              <div style="Margin-left:20px;Margin-right:20px">
+      <div class="m_6317060884218229271btn m_6317060884218229271btn--ghost m_6317060884218229271btn--large" style="Margin-bottom:20px;text-align:center">
+        <u></u><a style="border-radius:4px;display:inline-block;font-size:14px;font-weight:bold;line-height:24px;padding:12px 24px;text-align:center;text-decoration:none!important;color:#fff;border:1px solid #fff;font-family:Avenir,sans-serif" href="http://lantio.cmail20.com/t/d-l-djyuddt-bdulrydhh-j/" target="_blank" data-saferedirecturl="https://www.google.com/url?hl=ru&amp;q=http://lantio.cmail20.com/t/d-l-djyuddt-bdulrydhh-j/&amp;source=gmail&amp;ust=1477557075155000&amp;usg=AFQjCNEc9bud_5YpJo5rhFPeoiNBoKjIEA">ACTIVATE</a><u></u>
+      </div>
+    </div>
+            
+              <div style="Margin-left:20px;Margin-right:20px">
+      <div style="line-height:88px;font-size:1px">&nbsp;</div>
+    </div>
+            
+            </div>
+          
+          </div>
+        </div>
+      </div>
+  
+      <div style="line-height:30px;font-size:30px">&nbsp;</div>
+  
+      <div class="m_6317060884218229271layout m_6317060884218229271email-footer" style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 173000px);word-wrap:break-word;word-break:break-word">
+        <div class="m_6317060884218229271layout__inner" style="border-collapse:collapse;display:table;width:100%">
+        
+          <div class="m_6317060884218229271column m_6317060884218229271wide" style="text-align:left;font-size:12px;line-height:19px;color:#5c6266;font-family:Roboto,Tahoma,sans-serif;Float:left;max-width:400px;min-width:320px;width:320px;width:calc(8000% - 47600px)">
+            <div style="Margin-left:20px;Margin-right:20px;Margin-top:10px;Margin-bottom:10px">
+              
+              <div>
+              </div>
+              <div style="Margin-top:18px">
+                
+              </div>
+            </div>
+          </div>
+        
+          <div class="m_6317060884218229271column m_6317060884218229271narrow" style="text-align:left;font-size:12px;line-height:19px;color:#5c6266;font-family:Roboto,Tahoma,sans-serif;Float:left;max-width:320px;min-width:200px;width:320px;width:calc(72200px - 12000%)">
+            <div style="Margin-left:20px;Margin-right:20px;Margin-top:10px;Margin-bottom:10px">
+              
+            </div>
+          </div>
+        
+        </div>
+      </div>
+      <div class="m_6317060884218229271layout m_6317060884218229271one-col m_6317060884218229271email-footer" style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 173000px);word-wrap:break-word;word-break:break-word">
+        <div class="m_6317060884218229271layout__inner" style="border-collapse:collapse;display:table;width:100%">
+        
+          <div class="m_6317060884218229271column" style="text-align:left;font-size:12px;line-height:19px;color:#5c6266;font-family:Roboto,Tahoma,sans-serif;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px)">
+            <div style="Margin-left:20px;Margin-right:20px;Margin-top:10px;Margin-bottom:10px">
+              <div>
+              </div>
+            </div>
+          </div>
+        
+        </div>
+      </div>
+      <div style="line-height:40px;font-size:40px">&nbsp;</div>
+    </td></tr></tbody></table>
+  <img style="overflow:hidden;display:block!important;height:1px!important;width:1px!important;border:0!important;margin:0!important;padding:0!important" src="https://ci5.googleusercontent.com/proxy/DeR47G-HLdesdSHEXsSpZlYY4bSLl6zRtuhgnH0BwWjzyfEYLTtd25SNOpEjKSdi_JneluOQg5m-pXjPj3ewBjzVv33mWnkmUQUcUfBP=s0-d-e1-ft#https://lantio.cmail20.com/t/d-o-djyuddt-bdulrydhh/o.gif" width="1" height="1" border="0" alt="" class="CToWUd"><div class="yj6qo"></div><div class="adL">
+</div></div><div class="adL">
+
+
+</div></div>
