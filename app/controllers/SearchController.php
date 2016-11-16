9280fee58e9a06804d21071ac22816baf0f3ab34
Дмитрий <badarancha.dmitry@gmail.com>
2016-11-16 18:26:24 +0200

    Изменения модели и контроллера поиска

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -2,11 +2,9 @@
 
 class SearchController extends Controller {
     public function actionIndex() {
-        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-            $this->model->ajaxHandler();
-            exit;
+        if (isset($_POST['submit'])) {
+            $data = $this->model->getData();
         }
-
         $this->view->render('search');
     }
 }
