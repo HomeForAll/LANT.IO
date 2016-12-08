034681b01298500a8785a18c01b9b072d159ce63
Дмитрий <badarancha.dmitry@gmail.com>
2016-12-08 13:34:53 +0200

    Форматирование кода и испраление ошибок, для формы аренды комнаты

--- app/views/search.php
+++ app/views/search.php
@@ -798,6 +798,8 @@ echo '</pre>';
                 </div>
             </div>
     </fieldset>
+    
+    <input style="margin: 20px 0 50px 0" type="submit" name="apartRent" value="Найти">
 </form>
 
 <span style="font-size: 15pt;">
@@ -938,7 +940,7 @@ echo '</pre>';
                 </select>
                 <br>
                 <label for="photo">Фото:</label>
-                <select name="foto" id="foto" onchange="">
+                <select name="photo" id="photo" onchange="">
                     <option value="">---</option>
                     <option value="1">Есть</option>
                     <option value="2">Нет</option>
@@ -947,4 +949,373 @@ echo '</pre>';
             </div>
         </div>
     </fieldset>
+    
+    <input style="margin: 20px 0 50px 0" type="submit" name="apartRent" value="Найти">
+</form>
+
+<span style="font-size: 15pt;">
+    <strong>Аренда комнаты</strong>
+</span>
+<form action="/search" method="post">
+    
+    <input type="hidden" name="subject" value="room">
+    <input type="hidden" name="operation" value="rent">
+    
+    <fieldset>
+        <legend>Базовые параметры</legend>
+        <div style="margin: 15px">
+            Цена:
+            <div class="indent">
+                Стоимость:
+                <input name="minPrice" type="text" placeholder="Мин.">
+                <input name="maxPrice" type="text" placeholder="Макс.">
+                <br>
+                <label for="bargain">Торг:</label>
+                <select name="bargain" id="bargain">
+                    <option value="">---</option>
+                    <option value="1">Возможен</option>
+                    <option value="2">Не возможен</option>
+                </select>
+                <br>
+                <label for="rentType">Тип аренды:</label>
+                <select name="rentType" id="rentType">
+                    <option value="">---</option>
+                    <option value="1">Часовая</option>
+                    <option value="2">Посуточная</option>
+                    <option value="3">Долгосрочная</option>
+                </select>
+            </div>
+            
+            <label for="roomsperpay">Комнат в продажу (шт.):</label>
+            <select name="roomsperpay" id="roomsperpay">
+                <option value="">---</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+            </select>
+            
+            <br>
+            <br>
+    
+            Расположение:
+            <br>
+            <input type="text" id="search" placeholder="Адрес ..." style="padding: 10px; width: 477px; position: relative; left: 50%; margin: 0 0 0 -250px;" oninput="getGeoCoderData(this.value)" onkeyup="return false;">
+            <div id="map" style="position: relative; left: 50%; margin: 20px 0 0 -250px; width: 500px; height: 500px"></div>
+    
+            <div class="indent">
+                <label for="country">Страна:</label> <span id="country"></span>
+                <br>
+                <label for="area">Область:</label> <span id="area"></span>
+                <br>
+                <label for="city">Город:</label> <span id="city"></span>
+                <br>
+                <label for="region">Район:</label> <span id="region"></span>
+                <br>
+                <label for="street">Улица:</label> <span id="street"></span>
+                <br>
+                Станция метро:
+                <br>
+                <div class="indent">
+                    Удаленность от метро:
+                    <input type="text" name="metroMin" placeholder="Мин.">
+                    <input type="text" name="metroMax" placeholder="Макс.">
+                    <br>
+                </div>
+            </div>
+            
+            <label for="roomlocation">Нахождение комнаты:</label>
+            <select name="roomlocation" id="roomlocation">
+                <option value="">---</option>
+                <option value="1">Квартира</option>
+                <option value="2">Общежитие</option>
+                <option value="3">Частный дом</option>
+            </select>
+            <br>
+            <br>
+        </div>
+    </fieldset>
+    
+    <br>
+    <br>
+    
+    <fieldset>
+        <legend>Описание объекта</legend>
+        <div style="margin: 15px">
+            <strong>Описание комнаты:</strong>
+            <div class="indent">
+                Площадь:
+                <input type="text" name="spaceMin" placeholder="От">
+                <input type="text" name="spaceMax" placeholder="До">
+                <br>
+                Этаж:
+                <input type="text" name="floorMin" placeholder="От">
+                <input type="text" name="floorMax" placeholder="До">
+                <br>
+                <label for="equipment">Комплектация:</label>
+                <select name="equipment" id="equipment">
+                    <option value="">---</option>
+                    <option value="1">Укомплектованая</option>
+                    <option value="2">Пустая</option>
+                </select>
+                <br>
+                <label for="ceilingHeight">Высота потолков:</label>
+                <input type="text" name="ceilingHeight" id="ceilingHeight">
+            </div>
+            
+            Наполнение квартиры:
+            <div class="indent">
+                Электроника для досуга и отдыха:
+                <div class="indent">
+                    <label for="TV">Телевизор</label>
+                    <input type="checkbox" name="TV" id="TV">
+                    <br>
+                    <label for="musicCenter">Музыкльный центр</label>
+                    <input type="checkbox" name="musicCenter" id="musicCenter">
+                    <br>
+                    <label for="conditioner">Кондиционер</label>
+                    <input type="checkbox" name="conditioner" id="conditioner">
+                    <br>
+                </div>
+                
+                Бытовая техника:
+                <div class="indent">
+                    <label for="fridge">Холодильник</label>
+                    <input type="checkbox" name="fridge" id="fridge">
+                    <br>
+                    <label for="plate">Плита</label>
+                    <input type="checkbox" name="plate" id="plate">
+                    <br>
+                    <label for="bake">Печь</label>
+                    <input type="checkbox" name="bake" id="bake">
+                    <br>
+                    <label for="microwave">СВЧ</label>
+                    <input type="checkbox" name="microwave" id="microwave">
+                    <br>
+                    <label for="dishwasher">Посудомойка</label>
+                    <input type="checkbox" name="dishwasher" id="dishwasher">
+                    <br>
+                </div>
+                
+                Мебель:
+                <div class="indent">
+                    <label for="table">Стол</label>
+                    <input type="checkbox" name="table" id="table">
+                    <br>
+                    <label for="bed">Кровать</label>
+                    <input type="checkbox" name="bed" id="bed">
+                    <br>
+                    <label for="cupboard">Шкаф</label>
+                    <input type="checkbox" name="cupboard" id="cupboard">
+                    <br>
+                    <label for="stand">Тумба</label>
+                    <input type="checkbox" name="stand" id="stand">
+                    <br>
+                    <label for="mirror">Зеркало</label>
+                    <input type="checkbox" name="mirror" id="mirror">
+                    <br>
+                    <label for="armchair">Кресло</label>
+                    <input type="checkbox" name="armchair" id="armchair">
+                    <br>
+                    <label for="sofa">Диван</label>
+                    <input type="checkbox" name="sofa" id="sofa">
+                    <br>
+                </div>
+            </div>
+            
+            <div class="indent">
+                <label for="decoration">Отделка:</label>
+                <select name="decoration" id="decoration">
+                    <option value="">---</option>
+                    <option value="1">Да</option>
+                    <option value="2">Нет</option>
+                </select>
+                <select name="decorationValue">
+                    <option value="">---</option>
+                    <option value="1">Премиум</option>
+                    <option value="2">Стандартная</option>
+                </select>
+                <br>
+            </div>
+            
+            <label for="balcony">Балкон:</label>
+            <select name="balcony" id="balcony" onchange="">
+                <option value="">---</option>
+                <option value="1">Незастекленный</option>
+                <option value="2">Лоджия</option>
+            </select>
+            <br>
+            
+            <div style="margin: 15px">
+                <strong>Описание квартиры:</strong>
+                <div class="indent">
+                    <label for="houseType">Тип дома:</label>
+                    <select name="houseType" id="houseType" onchange="">
+                        <option value="">---</option>
+                        <option value="1">Блочный</option>
+                        <option value="2">Брежневка</option>
+                        <option value="3">Индивидуальный</option>
+                        <option value="4">Кирпично-монолитный</option>
+                        <option value="5">Монолит</option>
+                        <option value="6">Панельный</option>
+                        <option value="7">Сталинка</option>
+                        <option value="8">Хрущевка</option>
+                        <option value="9">Серия дома</option>
+                    </select>
+                    <br>
+                    
+                    <div class="indent">
+                        <label for="roomsNumber">Количество комнат:</label>
+                        <select name="roomsNumber" id="roomsNumber" onchange="">
+                            <option value="">---</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4+</option>
+                        </select>
+                        <br>
+                        
+                        Площадь:
+                        <input type="text" name="spaceMin" placeholder="От">
+                        <input type="text" name="spaceMax" placeholder="До">
+                        <br>
+                        
+                        Кухня:
+                        <input type="text" name="spaceMinKitchen" placeholder="От">
+                        <input type="text" name="spaceMaxKitchen" placeholder="До">
+                        <br>
+                        Этаж:
+                        <input type="text" name="floorMin" placeholder="От">
+                        <input type="text" name="floorMax" placeholder="До">
+                        <br>
+                        <label for="lavatory">Санузел:</label>
+                        <select name="lavatory" id="lavatory">
+                            <option value="">---</option>
+                            <option value="1">Совмещенный</option>
+                            <option value="2">Раздельный</option>
+                        </select>
+                        <br>
+                        Жилищно-комунальные услуги:
+                        <div class="indent">
+                            <label for="heating">Отопление</label>
+                            <input type="checkbox" name="heating" id="heating">
+                            <br>
+                            <label for="gas">Газ</label>
+                            <input type="checkbox" name="gas" id="gas">
+                            <br>
+                            <label for="electricity">Электричество</label>
+                            <input type="checkbox" name="electricity" id="electricity">
+                            <br>
+                            <label for="water">Водопровод</label>
+                            <input type="checkbox" name="water" id="water">
+                            <br>
+                        </div>
+                    </div>
+                </div>
+                <strong>Описание дома:</strong>
+                <div class="indent">
+                    <label for="houseType">Тип дома:</label>
+                    <select name="houseType" id="houseType" onchange="">
+                        <option value="">---</option>
+                        <option value="1">Блочный</option>
+                        <option value="2">Брежневка</option>
+                        <option value="3">Индивидуальный</option>
+                        <option value="4">Кирпично-монолитный</option>
+                        <option value="5">Монолит</option>
+                        <option value="6">Панельный</option>
+                        <option value="7">Сталинка</option>
+                        <option value="8">Хрущевка</option>
+                        <option value="9">Серия дома</option>
+                    </select>
+                    <br>
+                    
+                    <label for="TSJ">ТСЖ:</label>
+                    <select name="TSJ" id="TSJ">
+                        <option value="">---</option>
+                        <option value="1">Кооператив</option>
+                        <option value="2">Кондоминиум</option>
+                        <option value="3">Другое</option>
+                    </select>
+                    <br>
+                    
+                    <label for="houseFloorNumber">Количество этажей:</label>
+                    <input type="text" name="houseFloorNumber" id="houseFloorNumber">
+                    <br>
+                    <label for="lift">Лифт</label>
+                    <input type="checkbox" name="lift" id="lift">
+                    <br>
+                    <label for="stairs">Лестница</label>
+                    <input type="checkbox" name="stairs" id="stairs">
+                    <br>
+
+                    <br>
+                    <label for="parking">Парковка:</label>
+                    <select name="parking" id="parking" onchange="">
+                        <option value="">---</option>
+                        <option value="1">Подземная</option>
+                        <option value="2">Во дворе</option>
+                        <option value="3">Платная (неподалеку)</option>
+                    </select>
+                    <br>
+                    
+                    Безопасность:
+                    <div class="indent">
+                        <label for="concierge">Консьерж</label>
+                        <input type="checkbox" name="concierge" id="concierge">
+                        <br>
+                        <label for="security">Охрана</label>
+                        <input type="checkbox" name="security" id="security">
+                        <br>
+                        <label for="intercom">Домофон</label>
+                        <input type="checkbox" name="intercom" id="intercom">
+                        <br>
+                        <label for="CCTV">Видеонаблюдение</label>
+                        <input type="checkbox" name="CCTV" id="CCTV">
+                        <br>
+                    </div>
+                    
+                    <label for="chute">Мусоропровод:</label>
+                    <select name="chute" id="chute" onchange="">
+                        <option value="">---</option>
+                        <option value="1">Да</option>
+                        <option value="2">Нет</option>
+                    </select>
+                    <br>
+                </div>
+            </div>
+            
+            <strong>Вложения:</strong>
+            <div class="indent">
+                <label for="plan">План квартиры:</label>
+                <select name="plan" id="plan" onchange="">
+                    <option value="">---</option>
+                    <option value="1">Есть</option>
+                    <option value="2">Нет</option>
+                </select>
+                <br>
+                <label for="3d">3D проект:</label>
+                <select name="3d" id="3d" onchange="">
+                    <option value="">---</option>
+                    <option value="1">Есть</option>
+                    <option value="2">Нет</option>
+                </select>
+                <br>
+                <label for="video">Видео:</label>
+                <select name="video" id="video" onchange="">
+                    <option value="">---</option>
+                    <option value="1">Есть</option>
+                    <option value="2">Нет</option>
+                </select>
+                <br>
+                <label for="photo">Фото:</label>
+                <select name="photo" id="photo" onchange="">
+                    <option value="">---</option>
+                    <option value="1">Есть</option>
+                    <option value="2">Нет</option>
+                </select>
+                <br>
+            </div>
+        </div>
+    </fieldset>
+    
+    <input style="margin: 20px 0 50px 0" type="submit" name="apartRent" value="Найти">
 </form>
