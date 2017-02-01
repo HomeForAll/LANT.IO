3ebc426b08efed4f5c3f07f00b96fc92a4dd2579
web-yoga <hyehaeng@gmail.com>
2017-02-02 00:49:32 +0300

    повторный залив

--- app/views/cabinet.php
+++ app/views/cabinet.php
@@ -2,7 +2,13 @@
 $this->title = 'Личный кабинет';
 ?>
 <h1>Личный кабинет!</h1>
+<?php
+if($this->checkAccessLevel($_SESSION['status'], 'profile')){
+?>
 <a class="button" href="cabinet/profile/edit">Редактировать профиль</a>
+<?php
+}
+?>
 <a class="button" href="cabinet/generator">Генератор ключей</a>
 <a class="button" href="cabinet/keyeditor">Редактор ключей</a>
 <a class="button" href="cabinet/forms">Редактор форм</a>
