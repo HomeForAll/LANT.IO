3ed19ecc73ade0417e38ac7692e7ec77e00780e6
Дмитрий <badarancha.dmitry@gmail.com>
2017-02-25 13:27:18 +0200

    Адрес сокета.

--- templates/main/js.php
+++ templates/main/js.php
@@ -12,7 +12,7 @@ $user_id = isset($_SESSION['userID']) ? $_SESSION['userID'] : '';
 $email = isset($_SESSION['email']) ? $_SESSION['email'] : '';
 ?>
 <script>
-    var socket = io('http://localhost:3000?user_id=<?php echo $user_id; ?>&hash=<?php echo $hash; ?>');
+    var socket = io('http://beta.lant.io:3000?user_id=<?php echo $user_id; ?>&hash=<?php echo $hash; ?>');
 
     socket.on('connect', function(){
         console.log('Соединение установлено.');
