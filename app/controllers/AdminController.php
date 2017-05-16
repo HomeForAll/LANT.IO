b1f4baba48595f1fdb35066f0762ccec011b8065
hyehaeng@gmail.com <hyehaeng@gmail.com>
2017-05-16 21:41:38 +0300

    реструктуризация моделей и т.п.

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -2,13 +2,16 @@
 
 class AdminController extends Controller
 {
-    public function __construct($template, $modelName)
+    public function __construct($template)
     {
-        parent::__construct($template, $modelName);
+        parent::__construct($template);
         $this->checkAuth();
+        $this->setModel(new AdminModel());
+        $this->setModel(new NewsModel());
     }
 
-public function actionAdmin(){
+    public function actionAdmin()
+    {
 
         // Проверка доступа
         global $news_message, $news_error;
@@ -16,20 +19,20 @@ public function actionAdmin(){
         $news_message = [];
         $news_error = [];
         // Данные просмотра таблицы редактирования объявлений
-        $data = $this->model->getDataFromPostOrSession();
+        $data = $this->model('AdminModel')->getDataFromPostOrSession();
 
-        if(!empty($_POST['submit_status'])){
+        if (!empty($_POST['submit_status'])) {
             //Запись $_POST параметров в БД
-            $this->model->makeNewsStatus();
+            $this->model('NewsModel')->makeNewsStatus();
         }
 
         if (!empty($_POST['test2'])) {
-            $data['rabbitmq_message_newnews'] = Registry::model('news')->getNewNewsByRabbitMQ();
+            $data['rabbitmq_message_newnews'] = $this->model('NewsModel')->getNewNewsByRabbitMQ();
         }
 
-            $data['news'] = Registry::model('news')->
-            getRecentNewsList($data['time_start'], $data['time'], $data['max_number'], $data['space_type'],
-                $data['operation_type'], $data['object_type'], $data['best'], $data['status']);
+        $data['news'] = $this->model('NewsModel')->
+        getRecentNewsList($data['time_start'], $data['time'], $data['max_number'], $data['space_type'],
+            $data['operation_type'], $data['object_type'], $data['best'], $data['status']);
         $data['message'] = $news_message;
         $data['error'] = $news_error;
         $this->view->render('admin', $data);
@@ -45,14 +48,18 @@ public function actionAdmin(){
 
         $data = [];
         $form = [];
-        $data['message'] =[];
+        $data['message'] = [];
         // Получение данных для выбора форм
-        $data['form_options'] = $this->model->getFormOptionList();
+        $data['form_options'] = $this->model('AdminModel')->getFormOptionList();
 
         //Все уникальные элементы всех форм
         if (!empty($_POST['show_unique_forms_elements'])) {
-            if(isset($_POST['rus'])) { $rus = TRUE;} else {$rus = FALSE;}
-            $data['all_forms_elements'] = $this->model->getAllUniqueFormsElements($_POST['table'], $rus);
+            if (isset($_POST['rus'])) {
+                $rus = TRUE;
+            } else {
+                $rus = FALSE;
+            }
+            $data['all_forms_elements'] = $this->model('AdminModel')->getAllUniqueFormsElements($_POST['table'], $rus);
         }
         // Определение вида формы
         if (!empty($_POST['submit_add_news']) OR !empty($_POST['see_select_form'])) {
@@ -62,34 +69,34 @@ public function actionAdmin(){
             $object_types_id = (int)$_POST['object_types'];
 
             // Определение id формы
-            $form_id = $this->model->getFormID($space_types_id, $operation_types_id, $object_types_id);
+            $form_id = $this->model('AdminModel')->getFormID($space_types_id, $operation_types_id, $object_types_id);
 
             // Получение формы
-            $form = $this->model->getFormByID($form_id);
+            $form = $this->model('AdminModel')->getFormByID($form_id);
 
             $form['form_id'] = $form_id;
             $data['form'] = $form;
             $data['form'] = array_merge($data['form'],
-                $this->model->getFormBaseParamFromFormOptions($space_types_id, $operation_types_id, $object_types_id, $data['form_options']));
+                $this->model('AdminModel')->getFormBaseParamFromFormOptions($space_types_id, $operation_types_id, $object_types_id, $data['form_options']));
 
         }
         //Вспомогательная функция поиска формы по id элемента   !!!!!!
-        //$this->model->getFormNameByIdOfELEMENT($data['form_options']);
+        //$this->model('AdminModel')->getFormNameByIdOfELEMENT($data['form_options']);
 
         //Альтернативные данные из файла
- //       $data['form'] = $this->model->getFormFromFile('2_1_1.php');
+        // $data['form'] = $this->model('AdminModel')->getFormFromFile('2_1_1.php');
 
 
         //Вспомогательная функция поиска формы по id элемента   !!!!!!
-        if (!empty($_POST['show_same_name'])){
+        if (!empty($_POST['show_same_name'])) {
             $data['same_name'] = [];
             foreach ($data['form_options']['base'] as $help_id) {
-                $help_form = $this->model->getFormByID($help_id['id']);
-                array_push($data['same_name'], "<h3>Форма с id = ".$help_id['id']." [ "
-                    .$help_id['space_type']['r_name']
-                    ." - ".$help_id['object_type']['r_name']
-                    ." - ".$help_id['operation']['r_name']
-                    ." ]</h3>");
+                $help_form = $this->model('AdminModel')->getFormByID($help_id['id']);
+                array_push($data['same_name'], "<h3>Форма с id = " . $help_id['id'] . " [ "
+                    . $help_id['space_type']['r_name']
+                    . " - " . $help_id['object_type']['r_name']
+                    . " - " . $help_id['operation']['r_name']
+                    . " ]</h3>");
                 foreach ($help_form['same_name'] as $key => $value) {
                     if (isset($value)) {
                         array_push($data['same_name'], '<p style="color:red;">' . $key . '</p>');
@@ -102,75 +109,83 @@ public function actionAdmin(){
         }
 
         //Генерация данных для Таблицы Новостей в БД
-        $data['news_db'] = $this->model->generationNewsElements();
-
+        $data['news_db'] = $this->model('AdminModel')->generationNewsElements();
 
 
         //Исправления Индексов для сприсков
-        if (!empty($_POST['show_index_select_opt'])){
-            $data['show_index_select_opt'] = $this->model->getIndexSelectOpt();
+        if (!empty($_POST['show_index_select_opt'])) {
+            $data['show_index_select_opt'] = $this->model('AdminModel')->getIndexSelectOpt();
         }
 
         //Таблица новости в БД
-        if (!empty($_POST['generation_news_table'])){
-            $data['news_table'] = $this->model->generationNewsTable($data['news_db']);
+        if (!empty($_POST['generation_news_table'])) {
+            $data['news_table'] = $this->model('AdminModel')->generationNewsTable($data['news_db']);
         }
+        //Расхождения текущей БД со сгенерированной таблицой
+        if (!empty($_POST['generation_news_table_2'])) {
+            $data['news_table'] = $this->model('AdminModel')->generationNewsTable($data['news_db']);
+            $data['news_table_test'] = $this->model('AdminModel')->testNewsTable($data['news_table']);
+            if (!empty($data['news_table_test']['commands'])) {
+                $data['news_table_test_commands'] = $data['news_table_test']['commands'];
+            }
+            unset ($data['news_table_test']['commands']);
+            if (empty($data['news_table_test'])) {
+                $data['news_table_test'][0] = 'Различий не обнаружено!';
+            }
 
+        }
 
 
         //Генерация форм новостей на основе форм поиска
-        if (!empty($_POST['generating_news_forms_by_search_forms'])){
-            $data['news_form_generation_file'] = $this->model->generatingNewsFormsBySearchForms($data['news_db'], $_POST['generation_file_name']);
+        if (!empty($_POST['generating_news_forms_by_search_forms'])) {
+            $data['news_form_generation_file'] = $this->model('AdminModel')->generatingNewsFormsBySearchForms($data['news_db'], $_POST['generation_file_name']);
         }
         //Вставка php кода заполнения полей при редактировании
-        if (!empty($_POST['inserting_php_code_for_filling_fields'])){
-            $data['news_form_with_php_code'] = $this->model->insertingPhpCodeForFillingFields( $_POST['generation_file_name']);
+        if (!empty($_POST['inserting_php_code_for_filling_fields'])) {
+            $data['news_form_with_php_code'] = $this->model('AdminModel')->insertingPhpCodeForFillingFields($_POST['generation_file_name']);
         }
-
-
 
 
         //Генерировать $args - код фильтрации POST для getFormData модели news
-        if (!empty($_POST['generation_news_post_args'])){
-            $data['args'] = $this->model->generationNewsPostArgs($data['news_db']);
+        if (!empty($_POST['generation_news_post_args'])) {
+            $data['args'] = $this->model('AdminModel')->generationNewsPostArgs($data['news_db']);
         }
 
         //Генерировать массив всех checkbox и записать в NewsModel.php
-        if (!empty($_POST['all_checkbox_list'])){
-            $this->model->allCheckboxList($data['news_db']);
+        if (!empty($_POST['all_checkbox_list'])) {
+            $this->model('AdminModel')->allCheckboxList($data['news_db']);
         }
 
         // Список ВСЕХ элементов и опций (не уникальных)
         // для проверки всех вариантов
-        if (!empty($_POST['all_forms_elements_and_options'])){
+        if (!empty($_POST['all_forms_elements_and_options'])) {
             $data['all_forms_elements_and_options'] =
-                $this->model->getAllFormsElementsAndOptions($data['form_options']['base']);
+                $this->model('AdminModel')->getAllFormsElementsAndOptions($data['form_options']['base']);
         }
 
         // Установка input_type элементов в соотв. с типом данных БД (см. модели generationNewsElements())
         // И в соотв. с правилами и исключениями этой функции
-        if(isset($data['form']['category'])) {
+        if (isset($data['form']['category'])) {
             $data['form']['category'] =
-                $this->model->setInputTypeForElements($data['form']['category'], $data['news_db']);
+                $this->model('AdminModel')->setInputTypeForElements($data['form']['category'], $data['news_db']);
 
-        //Исправление нелепых данных в формах
-        $data['form']['category'] = $this->model->correctingStupidData($data['form']['category']);
+            //Исправление нелепых данных в формах
+            $data['form']['category'] = $this->model('AdminModel')->correctingStupidData($data['form']['category']);
 
         }
 
         // для проверки всех вариантов
-        if (!empty($_POST['elements_eng_rus'])){
+        if (!empty($_POST['elements_eng_rus'])) {
             $data['elements_eng_rus'] =
-                $this->model->elementsEngRus($data['news_db']);
+                $this->model('AdminModel')->elementsEngRus($data['news_db']);
         }
 
 
         //Внесение изменений в меню новостей в файле $f_name = news_myad.php
-        if(isset($_POST['change_news_menu'])) {
+        if (isset($_POST['change_news_menu'])) {
             $f_name = "app/views/news_myad.php";
-            array_push($data['message'],$this->model->changeFileNewsMenu($f_name, $data['form_options']));
+            array_push($data['message'], $this->model('AdminModel')->changeFileNewsMenu($f_name, $data['form_options']));
         }
-
 
 
         $this->view->render('news_form_generator', $data);
