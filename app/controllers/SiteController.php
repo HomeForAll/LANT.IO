5f637fd722f2697652d11adc5338c4f8f115ad08
hyehaeng@gmail.com <hyehaeng@gmail.com>
2017-05-19 17:21:07 +0300

    Ajax для "Лучшие объявления за 24 часа"

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -6,11 +6,34 @@ class SiteController extends Controller
     {
         parent::__construct($layout);
         $this->setModel(new SiteModel());
+         $this->setModel(new NewsModel());
     }
 
-    public function actionIndex()
+ public function actionIndex()
     {
-        $this->view->render('index');
+        $this->ifAJAX(function() {
+            if('top_apartments' == $_POST['action']){
+                $filter_result_arr = ['space_type', 'operation_type', 'object_type',
+                    'price_from', 'price_to', 'space_from', 'space_to'];
+                foreach($filter_result_arr as $name){
+                    if (isset($_POST[$name])) {
+                        $res[$name] = (int)$_POST[$name];
+                    } else {
+                        $res[$name] = 0;
+                    }
+                }
+                $data['best_news'] = $this->model('NewsModel')->getBestNewsOfTime(8064,$res['space_type'],
+                    $res['operation_type'], $res['object_type'], $res['price_from'], $res['price_to'],
+                    $res['space_from'], $res['space_to'], 9);
+                $data['best_news_number'] = $this->model('NewsModel')->getNamber_of_all_rows(8064,$res['space_type'],
+                    $res['operation_type'], $res['object_type'], $res['price_from'], $res['price_to'],
+                    $res['space_from'], $res['space_to']);
+                $this->model('NewsModel')->renderBestNewsOfTime($data['best_news'], $data['best_news_number']);
+            }
+        });
+        $data['best_news'] = $this->model('NewsModel')->getBestNewsOfTime(8064);
+        $data['best_news_number'] = $this->model('NewsModel')->getNamber_of_all_rows(8064);
+        $this->view->render('index', $data);
     }
 
     public function actionAccess()
