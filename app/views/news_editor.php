5b3e0c35331f8959c1455e42e984931652cde287
hyehaeng@gmail.com <hyehaeng@gmail.com>
2017-05-29 00:08:43 +0300

    админ панель, рейтинг лучшие, меню добавления новостей

--- app/views/news_editor.php
+++ app/views/news_editor.php
@@ -98,143 +98,140 @@ if (!empty($this->data['message'])) {
         echo '<span style="color: green">' . $message . '</span><br>';
     }
 }
-?>
 
-<form  id="editor_form" class="main_editor_form" enctype="multipart/form-data" action="" method="post">
-<input type="hidden" name="form_name" value="<?php echo $this->data['form_name']; ?>">
-<input type="hidden" name="space_type" value="<?php echo $this->data['space_type']; ?>">
-<input type="hidden" name="operation_type" value="<?php echo $this->data['operation_type']; ?>">
-<input type="hidden" name="object_type" value="<?php echo $this->data['object_type']; ?>">
-
-
-    
-
-
-    <fieldset>
-        <legend>Название</legend>
-        <section>
-            <label>Название новости*:
-                <input name="title" type="text" <?php inputToInput('title'); ?>>
-            </label>
-
-        </section>
-    </fieldset>
-
-    <!-- Карта -->
-
-
-    <!-- Вставляемые блоки -->
-    <?php
-    if (!empty($this->data['form_path'])) {
-        if ($this->data['form_path'] != 'base') {
-            include_once $this->data['form_path'];
-        } else {
-            echo '<input type="hidden" name="form_name" value="1">';
-        }
-    }
+// Вывод меню или формы
+if (!isset($this->data['form_name'])){
+    $this->model('NewsModel')->renderNewsEditorMenu();
+}else {
     ?>
 
+    <form id="editor_form" class="main_editor_form" enctype="multipart/form-data" action="" method="post">
+        <input type="hidden" name="form_name" value="<?php echo $this->data['form_name']; ?>">
+        <input type="hidden" name="space_type" value="<?php echo $this->data['space_type']; ?>">
+        <input type="hidden" name="operation_type" value="<?php echo $this->data['operation_type']; ?>">
+        <input type="hidden" name="object_type" value="<?php echo $this->data['object_type']; ?>">
 
 
+        <fieldset>
+            <legend>Название</legend>
+            <section>
+                <label>Название новости*:
+                    <input name="title" type="text" <?php inputToInput('title'); ?>>
+                </label>
+
+            </section>
+        </fieldset>
+
+        <!-- Карта -->
 
 
-    <!-- Долнительная информация -->
-    <fieldset>
-        <legend>Долнительная информация</legend>
-<!--        <section>-->
-<!--            <label>Короткое содержание: -->
-<!--                <textarea name="short_content" type="text">--><?php
-//                    if (!empty($this->data['short_content'])) {
-//                        echo $this->data['short_content'];
-//                    }
-//                    ?><!--</textarea>-->
-<!--            </label>-->
-<!--        </section>-->
-        <section>
-            <label >Cодержание:
-                <textarea id="news_content" name="content" type="text"><?php
-                    if (!empty($this->data['content'])) {
-                        echo $this->data['content'];
+        <!-- Вставляемые блоки -->
+        <?php
+        if (!empty($this->data['form_path'])) {
+            if ($this->data['form_path'] != 'base') {
+                include_once $this->data['form_path'];
+            } else {
+                echo '<input type="hidden" name="form_name" value="1">';
+            }
+        }
+        ?>
+
+
+        <!-- Долнительная информация -->
+        <fieldset>
+            <legend>Долнительная информация</legend>
+            <!--        <section>-->
+            <!--            <label>Короткое содержание: -->
+            <!--                <textarea name="short_content" type="text">--><?php
+            //                    if (!empty($this->data['short_content'])) {
+            //                        echo $this->data['short_content'];
+            //                    }
+            //                    ?><!--</textarea>-->
+            <!--            </label>-->
+            <!--        </section>-->
+            <section>
+                <label>Cодержание:
+                    <textarea id="news_content" name="content" type="text"><?php
+                        if (!empty($this->data['content'])) {
+                            echo $this->data['content'];
+                        }
+                        ?></textarea>
+                </label>
+            </section>
+
+            <section>
+                <label>Теги (через запятую):
+                    <input name="tags" type="text" value="<?php
+                    if (!empty($this->data['tags'])) {
+                        echo $this->data['tags'];
                     }
-                    ?></textarea>
-            </label>
-        </section>
+                    ?>">
+                </label>
+            </section>
 
-        <section>
-            <label>Теги (через запятую):
-                <input name="tags" type="text" value="<?php
-                       if (!empty($this->data['tags'])) {
-                           echo $this->data['tags'];
-                       }
-                       ?>">
-            </label>
-        </section>
-
-        <section>
-            <label>  Статус новости: 
-                <input type="radio" name="statusForUpdate" value="1" <?php
-                if (!empty($this->data['status'])) {
-                    if ($this->data['status'] == 1 or empty($this->data['id_news'])) {
-                        echo "checked";
+            <section>
+                <label> Статус новости:
+                    <input type="radio" name="statusForUpdate" value="1" <?php
+                    if (!empty($this->data['status'])) {
+                        if ($this->data['status'] == 1 or empty($this->data['id_news'])) {
+                            echo "checked";
+                        }
                     }
-                }
-                ?> > Публикация 
-                <input type="radio" name="statusForUpdate" value="0" <?php
-                if (!empty($this->data['status'])) {
-                    if ($this->data['status'] === 0) {
-                        echo "checked";
+                    ?> > Публикация
+                    <input type="radio" name="statusForUpdate" value="0" <?php
+                    if (!empty($this->data['status'])) {
+                        if ($this->data['status'] === 0) {
+                            echo "checked";
+                        }
                     }
-                }
-                ?> > Скрыть
-            </label>
-        </section>
-    </fieldset>
+                    ?> > Скрыть
+                </label>
+            </section>
+        </fieldset>
 
-    <!-- Ввод картинки -->
-    <fieldset>
-        <legend>Загрузка картинок</legend>
-        <section>
-            <div>Вы можете выбрать 5 файлов для загрузки. </div>
-            <div id="inputImageContainer">
+        <!-- Ввод картинки -->
+        <fieldset>
+            <legend>Загрузка картинок</legend>
+            <section>
+                <div>Вы можете выбрать 5 файлов для загрузки.</div>
+                <div id="inputImageContainer">
 
-                <div id="addDynamicField">
-                    <input type="button" id="addFieldButton" value="Добавить Фотографию:">
-                    <div id="imageMessage"><?php if (!empty($this->data["preview_img"][0])) { ?> Вы можете загрузить ещё: <?php  echo (5-count($this->data["preview_img"])); ?> фотографий. <?php } ?></div>
+                    <div id="addDynamicField">
+                        <input type="button" id="addFieldButton" value="Добавить Фотографию:">
+                        <div id="imageMessage"><?php if (!empty($this->data["preview_img"][0])) { ?> Вы можете загрузить ещё: <?php echo(5 - count($this->data["preview_img"])); ?> фотографий. <?php } ?></div>
+                    </div>
+
+                    <?php
+                    // Вывод уже имеющихся фотографий
+                    if (!empty($this->data["preview_img"][0])) {
+                        foreach ($this->data["preview_img"] as $k => $img) {
+                            $i = $k + 1; ?>
+                            <div class="imgInput imgInput_<?php echo $i; ?>">
+                                <label>Выберите фотографию: <?php echo $i; ?></label>
+                                <br>
+                                <img id="img-preview_<?php echo $i; ?>" src="/uploads/images/s_<?php echo $img; ?>">
+                                <input type="file" id="image_input_<?php echo $i; ?>"
+                                       name="image_name_<?php echo $i . '_saved_'; ?>" class="inputfile"
+                                       onchange="{document.getElementById(&quot;img-preview_<?php echo $i; ?>&quot;).src = window.URL.createObjectURL(this.files[0]);document.getElementById(&quot;image_input_<?php echo $i; ?>&quot;).setAttribute('name', &quot;image_name_<?php echo $i; ?>&quot;);}">
+                                <br>
+                                <input value="Удаление" type="button" class="DeleteField">
+                            </div>
+                            <?php
+                        }
+                    }
+                    ?>
+
+
                 </div>
-                
-           <?php 
-           // Вывод уже имеющихся фотографий
-            if (!empty($this->data["preview_img"][0])) {
-            foreach ($this->data["preview_img"] as $k => $img) {
-               $i = $k+1; ?>
-               <div class="imgInput imgInput_<?php echo $i; ?>">
-                    <label>Выберите  фотографию: <?php echo $i; ?></label>
-                <br>
-                <img id="img-preview_<?php echo $i; ?>" src="/uploads/images/s_<?php echo $img; ?>">
-                <input type="file" id="image_input_<?php echo $i; ?>" name="image_name_<?php echo $i.'_saved_'; ?>" class="inputfile" onchange="{document.getElementById(&quot;img-preview_<?php echo $i; ?>&quot;).src = window.URL.createObjectURL(this.files[0]);document.getElementById(&quot;image_input_<?php echo $i; ?>&quot;).setAttribute('name', &quot;image_name_<?php echo $i; ?>&quot;);}">
-                <br>
-                <input value="Удаление" type="button" class="DeleteField">
-            </div>
-           <?php    
-            } }
-           ?>
-            
-      
-
-            </div>
-        </section>
-    </fieldset>
+            </section>
+        </fieldset>
 
 
+        <input type="submit" name="submit_editor" value="Записать новость"> <a href="/news/editor">Отмена</a>
+    </form>
 
-    <input type="submit" name="submit_editor" value="Записать новость"> <a href="/news/editor">Отмена</a>
-</form>  
+    <?php } // Вывод меню или формы (конец)?>
 
-
-
-
-
-
-<script type="text/javascript" src="/template/js/jquery.validate.js"></script>
-<script type="text/javascript" src="/template/js/news_javascript.js"></script>
+    <script type="text/javascript" src="/template/js/jquery.validate.js"></script>
+    <script type="text/javascript" src="/template/js/news_javascript.js"></script>
 
--- app/views/news_id.php
+++ app/views/news_id.php
@@ -1,46 +1,5 @@
 <?php
-    // Установление куки о просмотренной странице
 
-$last_viewed_news_namber = 5; // 5 новостей
-$last_viewed_news_time = 0.00694; // время хранения (дней)
-
-$last_viewed_news_namber--;
-$last_viewed_news_time = $last_viewed_news_time*60*60*24;
-// массив для записи и сортировки COOKIE
-$last_viewed_news_array = [];
-
-
-$id_news = (int)$this->data['id_news'];
-
-if (isset($_COOKIE['last_viewed_news'])) {
-    //запись COOKIE в массив
-        for ($index = $last_viewed_news_namber; $index >= 0; $index--){
-        if(isset($_COOKIE['last_viewed_news'][$index])) {
-        $last_viewed_news_array[$index] = (int)$_COOKIE['last_viewed_news'][$index];
-
-        }
-    }
-    ksort($last_viewed_news_array);
-
-//Проверка существует ли уже новость в массиве
-$id_key = array_search($id_news, $last_viewed_news_array);
-if ($id_key !== FALSE){
-    // если да, то удаляем этот элемент
-    array_splice($last_viewed_news_array, $id_key, 1);
-}
-}
-
-// Добавляем id в начало массива
-array_unshift($last_viewed_news_array, $id_news);
-// Обрезаем массив
-array_splice($last_viewed_news_array, ($last_viewed_news_namber+1));
-
-// Запись COOKIE
-foreach ($last_viewed_news_array as $key => $value) {
-    $name = "last_viewed_news[$key]";
-    setcookie($name, $value, time()+$last_viewed_news_time);
-
-}
 ?>
 
 <?php
