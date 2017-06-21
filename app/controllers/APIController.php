014e97df7a618ddf9030b5ecf0cca50871e05b00
Alexander Medkov <azeeson@gmail.com>
2017-06-22 02:38:29 +0400

    Небольшие изменения контроллера API

--- app/controllers/APIController.php
+++ app/controllers/APIController.php
@@ -7,26 +7,26 @@ class APIController extends Controller
         parent::__construct($layout);
         $this->setModel(new SearchModel());
     }
-    
+
     public function actionSearch()
     {
         if (!empty($_GET)) {
             $this->model('SearchModel')->fetchAds($_GET);
-            echo json_encode($this->model('SearchModel')->getAds(), JSON_UNESCAPED_UNICODE);
         } elseif (!empty($_POST)) {
             $this->model('SearchModel')->fetchAds($_POST);
-            echo json_encode($this->model('SearchModel')->getAds(), JSON_UNESCAPED_UNICODE);
         }
+        echo json_encode($this->model('SearchModel')->getAds(), JSON_UNESCAPED_UNICODE);
     }
-    
+
     public function actionSearchAdsCount()
     {
         if (!empty($_GET)) {
             $this->model('SearchModel')->fetchAds($_GET, SearchModel::COUNT);
-            echo json_encode($this->model('SearchModel')->getAds(), JSON_UNESCAPED_UNICODE);
         } elseif (!empty($_POST)) {
             $this->model('SearchModel')->fetchAds($_POST, SearchModel::COUNT);
-            echo json_encode($this->model('SearchModel')->getAds(), JSON_UNESCAPED_UNICODE);
         }
+        $ads = $this->model('SearchModel')->getAds();
+        $ads = reset($ads);
+        echo json_encode($ads, JSON_UNESCAPED_UNICODE);
     }
 }
