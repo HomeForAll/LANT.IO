e58cd0b5af077eeec65b59c653644092b3cfa49f
Alexander Medkov <azeeson@gmail.com>
2017-07-27 10:48:43 +0400

    Clear project

--- template/layouts/web_build.php
+++ template/layouts/web_build.php
@@ -13,21 +13,8 @@ $siteModel = $this->model('SiteModel');
 <link rel="stylesheet" href="/template/css/main.css">
 </head>
 <body>
-<style type="text/css">
-#hellopreloader > #hellopreloader_preload {
-    position: fixed;z-index: 99999;
-    top: 0;left: 0;right: 0;bottom: 0;
-    width: 100vw;height: 100vh;
-    background-color: #292d48;
-    background: url('/template/images/puff.svg') center no-repeat,
-    url('/template/images/access_background.jpg') center no-repeat;
-    background-size: 123px, cover;
-}
-</style>
 <?php include_once ROOT_DIR . '/template/lucia/lucia.svg' ?>
 <div class="ax-loading"></div>
-<!-- 
-<div id="hellopreloader"><div id="hellopreloader_preload"></div></div> -->
 <div id="page" style="display: none">
 
 <?php include_once ROOT_DIR . '/template/html/login/menu.html' ?>
@@ -81,28 +68,6 @@ $siteModel = $this->model('SiteModel');
 </div>
 
 
-<!--
-<script type="text/javascript">
-    var hellopreloader = document.getElementById("hellopreloader_preload");
-    function fadeOutnojquery(el) {
-        el.style.opacity = 1;
-        var interhellopreloader = setInterval(function () {
-            el.style.opacity = el.style.opacity - 0.05;
-            if (el.style.opacity <= 0.05) {
-                clearInterval(interhellopreloader);
-                hellopreloader.style.display = "none";
-            }
-        }, 16);
-    }
-    window.onload = function () {
-        setTimeout(function () {
-            fadeOutnojquery(hellopreloader);
-        }, 1000);
-    };
-    document.getElementById('scene').style.display = "block";
-    document.getElementById('fullpage').style.display = "block";
-</script>
- -->
 <script src="/bower_components/jquery/dist/jquery.min.js"></script>
 <script src="/template/lucia/vendor.js"></script>
 <script src="/template/lucia/lucia.js"></script>
@@ -115,8 +80,5 @@ $siteModel = $this->model('SiteModel');
 <script src="/template/html/statistic/statistic.js"></script>
 <script src="/template/html/support_tickets/support_tickets.js"></script>
 
-
-<!-- <script src="/template/js/main_page/main.js"></script> -->
-<!-- <script src="/template/js/mainPageNewsAjax.js"></script> -->
 </body>
 </html>
